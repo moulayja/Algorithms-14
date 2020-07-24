51f11c83434bcda60d2701e5ddfd304c9256955e
Jamal <moulayjam@gmail.com>
2020-07-23 20:17:21 -0400

    Product of Digits of Sum [Solution]

--- Product of Digits of Sum.js
+++ Product of Digits of Sum.js
@@ -4,3 +4,5 @@ Create a function that takes numbers as arguments, adds them together,
 and returns the product of digits until the answer is only 1 digit long.
 */
 
+let prod = n => n < 10 ? n : prod(+String(n).split('').reduce((a,b)=>a*b));
+let sumDigProd = (...arr) => prod(arr.reduce((a,b)=>a+b));
